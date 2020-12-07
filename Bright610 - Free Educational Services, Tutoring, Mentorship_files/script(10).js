02cac378c5b8a3205772d2d04407d0cf76081093
Rachel <rachelzhou123@users.noreply.github.com>
2020-12-07 15:21:18 -0500

    the other data

---
+++ Bright610 - Free Educational Services, Tutoring, Mentorship_files/script(10).js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'bs-8',
+          componentName:'Gallery2',
+          props:props,
+          context:context,
+          contextKey:'context-bs-1',
+          radpack:false
+        });
+      })({"preset":"gallery2","id":"fcc29561-809a-4d79-8756-464d672a8f65","galleryImages":[{"image":{"width":"100%","left":"0%","height":"100%","position":"50% 50%","scale":0.08562918838421445,"editedAspectRatio":"1.906180193596426","overlayAlpha":0,"top":"0%","filter":"NONE","image":"\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fip\u002Ffc6a99ac-7269-46e5-928b-d032ebd621f5\u002Fab47d509-15a5-42c6-a59b-127a64c3adec%20(7).png","coordinates":{"x":0,"y":0},"rotation":"0"}}],"viewDevice":null,"staticContent":{"showMore":"Show More","demoCaption1":"Use captions to provide more information about your photos","demoCaption2":"Captions can prompt viewers to act. You can even insert a link","demoCaption3":"Use the caption to call out things the viewer may not notice","showLess":"Show Less"},"widgetId":"fcc29561-809a-4d79-8756-464d672a8f65","section":"default","category":"neutral","locale":"en-US","renderMode":"PUBLISH"},{"widgetId":"fcc29561-809a-4d79-8756-464d672a8f65","widgetType":"GALLERY","widgetPreset":"gallery2","order":2,"section":"default","category":"neutral","fontSize":"medium","fontFamily":"alternate","group":"Widget","groupType":"Banner","themeOverrides":{}});
